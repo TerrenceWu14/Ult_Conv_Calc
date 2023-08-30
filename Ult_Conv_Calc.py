d1bd609d8bd2ca6e9eee566e4f6f46414bdbd55f
TerrenceWu14 <135181592+TerrenceWu14@users.noreply.github.com>
2023-08-30 13:06:36 +1200

    Add files via upload

--- Ult_Conv_Calc.py
+++ Ult_Conv_Calc.py
@@ -1,11 +1,60 @@
 # Functions go here
 
+# Checks that the number entered is more than 0
+def num_check(question1: object):
+    # checks if the user entered a number that is more than zero
+    valid = False
+    while not valid:
+
+        error = "Please enter a number that is more than zero and lower (or equal to) than 200 (no decimals)"
+
+        try:
+
+            # asks user to enter a number
+            response: int = int(input(question1))
+
+            # checks if a number is above zero
+            if response > 0:
+                return response
+
+            # Outputs error if number is invalid
+            else:
+                print(error)
+                print()
+
+        except ValueError:
+            print(error)
+
+
 # Puts a series of symbols at the start and end of text
+def statement_generator(text, decoration):
+    # Make string with five characters
+    ends = decoration * 5
+
+    # Add decoration to start and end of statement
+    statement = "{} {} {}".format(ends, text, ends)
+
+    print()
+    print(statement)
+    print()
+
+    return ""
 
 
 # Displays instructions for new users
+def instructions():
+    statement_generator("Instructions / Information", "=")
+    print()
+    print("Please choose one of the following options to convert:"
+          " Measurements, Mass or Time ")
+    print("Please only type a number above 0 (with no decimals)")
+    print("Complete as many calculations as necessary, "
+          "Pressing <enter> at the end of each calculator or any key to quit.")
+    print()
+    return ""
 
 
+# Asks the user if they which calculation they want to do
 def user_choice():
     valid = False
 
@@ -15,7 +64,7 @@ def user_choice():
         # a list of valid responses
         time_ok = ["time", "t"]
         mass_ok = ["mass", "weight"]
-        measure_ok = ["measurement", "measure"]
+        measure_ok = ["measurements", "measure", "measurement"]
 
         # checks for valid response and returns text, integer or image
         if response in time_ok:
@@ -32,13 +81,21 @@ def user_choice():
                 return "measurement"
         else:
             # if response is not valid (in the lists), output an error
-            print(" Sorry, please choose to calculate time, mass or measurements")
+            print("Sorry, please choose to calculate time, mass or measurements")
             print()
 
 
 # Main routine
+statement_generator("Ultimate Conversion Calculator", "*")
+first_time = input("Press <enter> for instructions on how to use the calculator"
+                   " otherwise any other key to continue")
+
 keep_going = ""
 while keep_going == "":
+    if first_time == "":
+        instructions()
     calc_choice = user_choice()
     print("You chose", calc_choice)
-
+    print()
+    keep_going = input("Press <enter> to continue or any key to stop")
+    print()
