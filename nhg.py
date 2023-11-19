b1d683ee2b76d9de2d6407356277cd6d5c4f2a66
TerrenceWu14 <135181592+TerrenceWu14@users.noreply.github.com>
2023-11-20 10:01:59 +1300

    Add files via upload

--- nhg.py
+++ nhg.py
@@ -1,1 +1,25 @@
+# changing valid answers into unit abbreviation
+def to_abbreviation(user_unit, valid_list):
+    while True:
+        for item in valid_list:
+            print("item in list", item)
+            if user_unit in item:
+                print("possible unit", user_unit)
+                return item[0]
 
+        print("oops, you have not chosen a valid unit")
+        return "error"
+
+
+# main routine
+
+distance_list = [
+    ["mm", "millimeters", "millimeter"],
+    ["cm", "centimeters", "centimeter"],
+    ["m", "meters", "meter"],
+    ["km", "kilometers", "kilometer"],
+]
+
+var_unit = input("Enter Unit")
+checked_unit = to_abbreviation(var_unit, distance_list)
+print("checked unit", checked_unit)
